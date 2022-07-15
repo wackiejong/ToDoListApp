20b08671a0995226a28ff83e9446e3479b841814
Jackie Wong <jcwong91@gmail.com>
2022-07-15 15:41:11 -0400

    Iteration 0

---
+++ ToDoListApp/TableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TableViewController.swift
+//  ToDoListApp
+//
+//  Created by Jackie Wong on 7/15/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      toDos = createToDos()
+    }
+
+    func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]
+    }
+    
+    // MARK: - Table view data source
+
+    
+
+    //keep
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    //keep
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+          } else {
+            cell.textLabel?.text = toDo.name
+          }
+        
+        return cell
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+     
+     //keep
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Jackie Wong on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
+
